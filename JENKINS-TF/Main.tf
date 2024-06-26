c440e5a64967542a93c44620d47cb527981a6821
Atharva Nawathe <atharva.nawathe@innovapptive.com>
2024-06-26 12:26:26 +0530

    Edited terraform files

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  ami                    = "ami-0705384c0b33c194c"
+  instance_type          = "t3.large"
+  key_name               = "Pipeline"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
